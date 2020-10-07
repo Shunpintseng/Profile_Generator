c99190ec5c647c6939975723898688856be78df3
shunpin tseng <shunpin@gmail.com>
2020-10-06 20:58:49 -0700

    employee Update

--- Develop/lib/Employee.js
+++
@@ -1,9 +0,0 @@
-// TODO: Write code to define and export the Employee class
-class employee {
-    constructor(name, id, email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
-
